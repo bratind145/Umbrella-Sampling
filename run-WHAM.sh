39829977f0eeef2655fe650d2021871946759d53
Bratin Kumar Das <106652080+bratind145@users.noreply.github.com>
2025-08-08 09:47:44 +0530

    Update run-WHAM.sh

--- run-WHAM.sh
+++ run-WHAM.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Set output filenames
+tpr_output="tpr-files.dat"
+pullf_output="pullf-files.dat"
+
+# Gather and sort .tpr files
+printf "%s\n" md-um*.tpr | sort > "$tpr_output"
+
+# Gather and sort _pullf.xvg files
+printf "%s\n" md-um*_pullf.xvg | sort > "$pullf_output"
+
+echo "Sorted filenames written to $tpr_output and $pullf_output."
